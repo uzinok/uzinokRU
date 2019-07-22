b25ecdd43b189b145135826edf19cceeae8445fe
uzinok_HP <uzinok@yandex.ru>
2019-07-22 18:33:32 +0400

    готов скрипт scroll.js

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -5,17 +5,24 @@ function scroll() {
 
     // в цикле отслеживаем клик по каждой ссылке
     links.forEach(link => {
+
       link.addEventListener("click", function (e) {
         // отменяем действие по умолчанию
         e.preventDefault();
         // вызываю функцию скролла
         // передаем блок к которому будем скролить
-        to_go_scroll(document.querySelector(this.getAttribute("href")));
+        go_scroll(document.querySelector(this.getAttribute("href")));
       })
+
     });
 
-    function to_go_scroll(to_block) {
-      console.log("scroll");
+    function go_scroll(to_block) {
+
+      to_block.scrollIntoView({
+        block: "start",
+        behavior: "smooth"
+      });
+
     }
 
   }
