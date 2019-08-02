b3c344346056c42d9bd2275edcd64858b729e73c
uzinok_HP <uzinok@yandex.ru>
2019-08-02 19:18:49 +0400

    мелкие правки стилей по ленду

--- src/js/popup-contact.js
+++ src/js/popup-contact.js
@@ -1,16 +1,20 @@
 function popupContact() {
-  if (document.querySelector(".popup-btn")){
+  if (document.querySelector(".popup-btn")) {
 
     document.querySelector(".popup-btn").addEventListener("click", openPopup);
 
-    function openPopup() {
+    function openPopup(e) {
+      e.preventDefault();
       document.querySelector(".popup-contact").parentElement.hidden = false;
 
       document.querySelector(".popup__close").addEventListener("click", closePopup);
+
+      return false;
 
       function closePopup() {
         document.querySelector(".popup-contact").parentElement.hidden = true;
       }
     }
   }
+
 }
